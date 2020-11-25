b466ea5eb806eecd74e02e0e3882eb6bb51f7b53
EYLeong <leong.enyi.1997@gmail.com>
2020-11-25 23:35:55 +0800

    reset files

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("static"));
-
-app.get("/", (req, res) => {
-	console.log(req.query);
-	res.send("Hello World!");
-});
+app.use(express.static("static", {
+	index: 'randomtext.txt'
+}));
 
 app.listen(port, () =>
 	console.log(`App listening at http://localhost:${port}`)
